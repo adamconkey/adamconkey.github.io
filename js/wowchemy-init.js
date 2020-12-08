c05c87057e7380e130a0d62a07160e433a9b8abd
adamconkey <adam.conkey@utah.edu>
2020-12-08 12:53:12 -0700

    rebuilding site Tue Dec  8 12:53:12 MST 2020

--- js/wowchemy-init.js
+++ js/wowchemy-init.js
@@ -1,5 +1,5 @@
 (() => {
-  // ns-hugo:/var/folders/pr/c645lk715r76xrqz2q7bhjr80000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20201125230219-3a03b728de8f/assets/js/wowchemy-theming.js
+  // ns-hugo:/var/folders/pr/c645lk715r76xrqz2q7bhjr80000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20201205224343-d70da8919962/assets/js/wowchemy-theming.js
   function getThemeMode() {
     return parseInt(localStorage.getItem("dark_mode") || 2);
   }
